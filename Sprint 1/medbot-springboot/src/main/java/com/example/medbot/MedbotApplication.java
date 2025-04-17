f69157a9d3454fe0c02860807876e3453cda3702
helloAllyj <98562086+helloAllyj@users.noreply.github.com>
2025-04-17 11:25:16 -0400

    Add files via upload

---
+++ Sprint 1/medbot-springboot/src/main/java/com/example/medbot/MedbotApplication.java
@@ -0,0 +1,15 @@
+package com.example.medbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedbotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MedbotApplication.class, args);
+    }
+
+}
+
+
---
+++ Sprint 1/medbot-springboot/src/test/java/com/example/medbot/MedbotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.medbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedbotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
